977bb26e5a7740a14f412335065ce635d503f9e2
Salnik Valeriia <melfed.salnik@gmail.com>
2022-04-11 13:35:26 +0200

    Initial Commit

---
+++ TestJSONApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestJSONApp
+//
+//  Created by Валерия Смецкая on 11.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
