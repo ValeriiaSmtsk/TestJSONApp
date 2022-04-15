bd4d2677b8f0a4dff5950f735e879e8d427e13d8
Salnik Valeriia <melfed.salnik@gmail.com>
2022-04-15 19:15:03 +0200

    Work with interface

---
+++ TestJSONApp/CollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  CollectionViewCell.swift
+//  TestJSONApp
+//
+//  Created by Валерия Смецкая on 11.04.2022.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+}
